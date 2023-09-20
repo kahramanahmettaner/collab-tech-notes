7e9b4e278c73a6caa33846e555559699067791aa
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-09-20 16:05:29 +0200

    Enhance error handling and add custom 404 route
    Updated server.js to improve error handling, including the addition of a custom 404 route ('/404') and the '404.html' file for error responses.

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,22 @@ const PORT = process.env.PORT || 3500
 // Tell express where to find static files like css, images etc.
 app.use('/', express.static(path.join(__dirname, '/public')))
 
+// Use the 'root' route defined in 'root.js'
 app.use('/', require('./routes/root'))
 
+// Handle 404 errors
+app.all('*', (req, res) => {
+    res.status(404)
+
+     // Check the 'Accept' header to respond with HTML, JSON, or plain text
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
+})
+
+// Start the Express server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
