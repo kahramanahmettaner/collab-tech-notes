30df1bef681f3fba4137cec29929c80a492b7b07
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-09-23 15:54:29 +0200

    Implement routing functionality and add facing page and login component
    The routing functionality for the React application has been implemented using React Router. The components Public.js Login.js Login.js have been added with basic code but not fully implemented. Additionally, Basic style for the pages has been added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
+import { Routes, Route } from "react-router-dom"
+import Layout from "./components/Layout"
+import Public from "./components/Public"
+import Login from "./features/auth/Login"
 
 function App() {
   return (
-    <div className="App">
-      <h1> Welcome! </h1>
-    </div>
+      <Routes >
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Public />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
   );
 }
 
