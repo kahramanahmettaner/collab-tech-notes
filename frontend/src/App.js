7534077cfbed612a041387b4cc20b944ea1f942e
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-09-23 16:03:14 +0200

    Add 'dash' route with new components and styling
    This commit introduces a new route, dash, to the application along with associated components Welcome.js, DashLayout.js, DashHeader.js and DashFooter.js and basic styles for these elements. These additions enhance the application's structure and prepare for future development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Public from "./components/Public"
 import Login from "./features/auth/Login"
+import DashLayout from "./components/DashLayout"
+import Welcome from "./features/auth/Welcome"
 
 function App() {
   return (
@@ -9,6 +11,11 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Public />} />
           <Route path="login" element={<Login />} />
+
+          <Route path="dash" element={<DashLayout />}>
+            <Route index element={<Welcome />} />
+          </Route>
+        
         </Route>
       </Routes>
   );
