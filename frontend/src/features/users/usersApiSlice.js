ca3de8dfece48e537974faa48f74665bb103ef95
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-11-06 23:56:01 +0100

    Introduce usersSlice.js for User Data Management with RTK Query
    -m- Created 'usersSlice.js' to manage user data using @reduxjs/toolkit
    
    - Integrated user data API endpoints and normalized data handling within the slice
    
    - Implemented RTK Query endpoints for fetching user data from '/users'
    
    - Utilized selectors for querying and accessing normalized user data

---
+++ frontend/src/features/users/usersApiSlice.js
@@ -0,0 +1,50 @@
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit"
+import { apiSlice } from "../../app/api/apiSlice"
+
+const usersAdapter = createEntityAdapter({})
+
+const initialState = usersAdapter.getInitialState()
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        getUsers: builder.query({
+            query: () => '/users',
+            validateStatus: (response, result) => {
+                return response.status === 200 && !result.isError // even if response.status === 200 there could be an error
+            },
+            keepUnusedDataFor: 5, // default is 60 secs and more suitable for deployment
+            transformResponse: responseData => {
+                const loadedUsers = responseData.map(user => {
+                    user.id = user._id // we use normalized data and it expects an id property 
+                    return user
+                })
+                return usersAdapter.setAll(initialState, loadedUsers)
+            },
+            providesTags: (result, error, arg) => {
+                if (result?.ids) {
+                    return [
+                        { type: 'User', id: 'LIST' },
+                        ...result.ids.map(id => ({ type: 'User', id }))
+                    ]
+                } else return [{ type: 'User', id: 'LIST' }]
+            }
+        })
+    })
+})
+
+// rtk query generates hooks based on the endpoints
+export const { useGetUsersQuery } = usersApiSlice
+
+// returns the query result object
+export const selectUsersResult = usersApiSlice.endpoints.getUsers.select()
+
+// creates memoized selector
+const selectUsersData = createSelector(selectUsersResult, usersResult => usersResult.data) // normalized state object with ids & entities
+
+// getSelectors creates these selectors and we rename them with aliases using destructuring
+export const {
+    selectAll: selectAllUsers,
+    selectById: selectUserById,
+    selectIds: selectUserIds
+    // Pass in a selector that returns the users slice of state
+} = usersAdapter.getSelectors(state => selectUsersData(state) ?? initialState)
