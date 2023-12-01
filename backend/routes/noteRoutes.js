3e6a49b3a999fac9cbea7e8357ee4426a6c4c967
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-12-01 23:44:01 +0100

    Implement JWT verification middleware
    - Introduce a new middleware, , responsible for authenticating requests using JSON Web Tokens (JWT)
    
    - Applied this middleware to both note and user routes to enforce secure access control
    
    - This enhancement ensures that only authenticated users with valid JWTs can interact with the specified routes

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const notesController = require('../controllers/notesController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+router.use(verifyJWT) // Middleware applied to all routes in this file
 
 router.route('/') // this will match '/notes' 
     .get(notesController.getAllNotes)
