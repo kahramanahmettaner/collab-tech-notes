2088e75a18741e74886a1c80c3e54a3cd355940a
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-12-07 23:28:49 +0100

    Update DashHeader.js layout and functionality
    - Updated the layout to include a logout button in the navigation
    
    - Implemented dynamic class assignment based on the current route for styling adjustments
    
    - Utilized useEffect to navigate to the home page on successful logout
    
    - Handled loading and error states during the logout process

--- frontend/src/components/DashHeader.js
+++ frontend/src/components/DashHeader.js
@@ -1,15 +1,52 @@
-import { Link } from "react-router-dom"
+import { useEffect } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
+import { useSendLogoutMutation } from '../features/auth/authApiSlice'
+
+const DASH_REGEX = /^\/dash(\/)?$/
+const NOTES_REGEX = /^\/dash\/notes(\/)?$/
+const USERS_REGEX = /^\/dash\/users(\/)?$/
 
 const DashLayout = () => {
 
+    const navigate = useNavigate()
+    const { pathname } = useLocation()
+
+    const [sendLogout, { isLoading, isSuccess, isError, error }] = useSendLogoutMutation()
+
+    useEffect(() => { if (isSuccess) navigate('/') }, [isSuccess, navigate])
+
+    const onLogoutClicked = () => sendLogout()
+
+    if (isLoading) return <p>Logging Out...</p>
+
+    if (isError) return <p>Error: { error.data?.message }</p>
+
+    let dashClass = null
+    if (!DASH_REGEX.test(pathname) && !NOTES_REGEX.test(pathname) && !USERS_REGEX.test(pathname)) {
+        dashClass = 'dash-header__container--mail'
+    }
+
+    const logotButton = (
+        <button
+            className="icon-button"
+            title="Logout"
+            onClick={onLogoutClicked}
+        >
+            <FontAwesomeIcon icon={faRightFromBracket} />
+        </button>
+    )
+
     const content = (
         <header className="dash-header">
-            <div className="dash-header__container">
+            <div className={`dash-header__container ${dashClass}`}>
                 <Link to="/dash">
                     <h1 className="dash-hedaer__title">CollabTechNotes</h1>
                 </Link>
                 <nav className="dash-header__nav">
-                    {/* add nav buttons later */}
+                    {/* add more buttons later */}
+                    {logotButton}
                 </nav>
             </div>
         </header>
