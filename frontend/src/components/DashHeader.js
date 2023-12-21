3e8e25838e808eb1b2d88abe97e9a6fc0e8aae7c
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-12-21 09:34:28 +0100

    Enhance DashHeader.js for improved functionality and user experience
    - Utilize useAuth hook to dynamically display buttons based on user roles
    
    - Implement new buttons for creating notes and users, and navigating to notes and users lists´-m- Display appropriate buttons based on the current location in the dashboard

--- frontend/src/components/DashHeader.js
+++ frontend/src/components/DashHeader.js
@@ -1,14 +1,17 @@
-import { useEffect } from "react"
+import { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { faFileCirclePlus, faFilePen, faUserGear, faUserPlus, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import { Link, useNavigate, useLocation } from 'react-router-dom'
 import { useSendLogoutMutation } from '../features/auth/authApiSlice'
+import useAuth from '../hooks/useAuth'
 
 const DASH_REGEX = /^\/dash(\/)?$/
 const NOTES_REGEX = /^\/dash\/notes(\/)?$/
 const USERS_REGEX = /^\/dash\/users(\/)?$/
 
 const DashHeader = () => {
+
+    const { isManager, isAdmin } = useAuth()
 
     const navigate = useNavigate()
     const { pathname } = useLocation()
@@ -17,15 +20,71 @@ const DashHeader = () => {
 
     useEffect(() => { if (isSuccess) navigate('/') }, [isSuccess, navigate])
 
+    const onNewNoteClicked = () => navigate('/dash/notes/new')
+    const onNewUserClicked = () => navigate('/dash/users/new')
+    const onNotesClicked = () => navigate('/dash/notes')
+    const onUsersClicked = () => navigate('/dash/users')
+
     const onLogoutClicked = () => sendLogout()
-
-    if (isLoading) return <p>Logging Out...</p>
-
-    if (isError) return <p>Error: { error.data?.message }</p>
 
     let dashClass = null
     if (!DASH_REGEX.test(pathname) && !NOTES_REGEX.test(pathname) && !USERS_REGEX.test(pathname)) {
         dashClass = 'dash-header__container--mail'
+    }
+
+    let newNoteButton = null
+    if (NOTES_REGEX.test(pathname)) {
+        newNoteButton = (
+            <button
+                className='icon-button'
+                title='New Note'
+                onClick={onNewNoteClicked}
+            >
+                <FontAwesomeIcon icon={faFileCirclePlus} />
+            </button>
+        )
+    }
+
+    let newUserButton = null
+    if (USERS_REGEX.test(pathname)) {
+        newUserButton = (
+            <button
+                className='icon-button'
+                title='New User'
+                onClick={onNewUserClicked}
+            >
+                <FontAwesomeIcon icon={faUserPlus} />
+            </button>
+        )
+    }
+
+    let notesButton = null
+    // we are not in notes list page && in one of the dash pages
+    if (!NOTES_REGEX.test(pathname) && pathname.includes('/dash')) { 
+        notesButton = (
+            <button
+                className='icon-button'
+                title='Notes'
+                onClick={onNotesClicked}
+            >
+                <FontAwesomeIcon icon={faFilePen} />
+            </button>
+        )
+    }
+
+    let usersButton = null
+    if(isManager || isAdmin) {
+        if (!USERS_REGEX.test(pathname) && pathname.includes('/dash')) {
+            usersButton = (
+                <button
+                    className='icon-button'
+                    title='Users'
+                    onClick={onUsersClicked}
+                >
+                    <FontAwesomeIcon icon={faUserGear} />
+                </button>
+            )
+        }
     }
 
     const logotButton = (
@@ -38,18 +97,40 @@ const DashHeader = () => {
         </button>
     )
 
+    const errClass = isError ? 'errmsg': 'offscreen'
+
+    let buttonContent
+
+    if (isLoading) {
+        buttonContent = <p>Logging Out...</p>
+    } else {
+        buttonContent = (
+            <>
+                {newNoteButton}
+                {newUserButton}
+                {notesButton}
+                {usersButton}
+                {logotButton}
+            </>
+        )
+    }
+
+
     const content = (
-        <header className="dash-header">
-            <div className={`dash-header__container ${dashClass}`}>
-                <Link to="/dash">
-                    <h1 className="dash-hedaer__title">CollabTechNotes</h1>
-                </Link>
-                <nav className="dash-header__nav">
-                    {/* add more buttons later */}
-                    {logotButton}
-                </nav>
-            </div>
-        </header>
+        <>
+            <p className={errClass}>{error?.data?.message}</p>
+          
+            <header className="dash-header">
+                <div className={`dash-header__container ${dashClass}`}>
+                    <Link to="/dash">
+                        <h1 className="dash-hedaer__title">CollabTechNotes</h1>
+                    </Link>
+                    <nav className="dash-header__nav">
+                        {buttonContent}
+                    </nav>
+                </div>
+            </header>
+        </>
     )
     
     return content
