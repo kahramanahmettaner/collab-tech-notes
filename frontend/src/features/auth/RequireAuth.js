e43a3b70e3443460f62dc03f97823d71a252326b
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-12-22 22:26:09 +0100

    Implement RequireAuth component for role-based access control
    - Introduces a new component, RequireAuth, to enforce authentication based on user roles
    
    - Takes an 'allowedRoles' prop to specify the roles allowed to access the content
    
    - Renders the child components (Outlet) if the user has the required roles
    
    - Redirects to the login page if the user does not have the required roles
    
    - Enhances role-based access control for protected routes in the application

---
+++ frontend/src/features/auth/RequireAuth.js
@@ -0,0 +1,18 @@
+import { useLocation, Navigate, Outlet  } from 'react-router-dom'
+import useAuth from '../../hooks/useAuth'
+
+const RequireAuth = ({ allowedRoles }) => {
+
+    const location = useLocation()
+    const { roles } = useAuth()
+
+    const content = (
+        roles.some( role => allowedRoles.include(role) )
+            ? <Outlet />
+            : <Navigate to='/login' state={{ from: location }} replace /> // removes the RequireAuth component from the history
+    )
+
+    return content
+}
+
+export default RequireAuth
