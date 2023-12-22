32b55d77ff65ded8bc8a1db9bf8e45b0e4fc9717
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-12-22 22:45:14 +0100

    Integrate role-based authentication in App.js
    - Implemented protected routes using RequireAuth component to control access based on user roles
    
    - Defined public routes for unauthenticated access
    
    - Utilized the ROLES configuration from /config/roles for allowedRoles in RequireAuth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,33 +12,48 @@ import EditNote from "./features/notes/EditNote"
 import NewNote from "./features/notes/NewNote"
 import Prefetch from "./features/auth/Prefetch"
 import PersistLogin from "./features/auth/PersistLogin"
+import RequireAuth from "./features/auth/RequireAuth"
+import { ROLES } from "./config/roles"
 
 function App() {
   return (
       <Routes >
         <Route path="/" element={<Layout />}>
+          
+          {/* Public Routes */}
           <Route index element={<Public />} />
           <Route path="login" element={<Login />} />
 
+          {/* Protected Routes */}          
           <Route path="" element={<PersistLogin />} >
-            <Route path="" element={<Prefetch />} >
-              <Route path="dash" element={<DashLayout />}>
-                <Route index element={<Welcome />} />
-                
-                <Route path="notes">
-                  <Route index element={<NotesList />} />
-                  <Route path=':id' element={<EditNote />} />
-                  <Route path='new' element={<NewNote />} />
-                </Route>
-                
-                <Route path="users">
-                  <Route index element={<UsersList />} />
-                  <Route path=':id' element={<EditUser />} />
-                  <Route path='new' element={<NewUserForm />} />
-                </Route>
-              </Route> {/* End Dash */}
-            </Route> {/* End Prefetch */}
+
+            {/* Users with any role can access */}
+            <Route path="" element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />} >
+              <Route path="" element={<Prefetch />} >
+                <Route path="dash" element={<DashLayout />}>
+                  <Route index element={<Welcome />} />
+                  
+
+                  <Route path="notes">
+                    <Route index element={<NotesList />} />
+                    <Route path=':id' element={<EditNote />} />
+                    <Route path='new' element={<NewNote />} />
+                  </Route>
+                  
+                  {/* Only Managers and Admins can access */}
+                  <Route path="" element={<RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />} >
+                    <Route path="users">
+                      <Route index element={<UsersList />} />
+                      <Route path=':id' element={<EditUser />} />
+                      <Route path='new' element={<NewUserForm />} />
+                    </Route>
+                  </Route>
+
+                </Route> {/* End Dash */}
+              </Route> {/* End Prefetch */}
+            </Route>    
           </Route> {/* End PersistLogin */}
+          {/* End Protected Routes */}
 
         </Route> {/* End Layout */}
 
