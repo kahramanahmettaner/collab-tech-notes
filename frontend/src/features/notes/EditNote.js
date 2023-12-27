f30074f3b28de23bdaa54d6b105ef997a582f092
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2023-12-28 00:09:53 +0100

    Fix EditUser.js, NewNote.js, and EditNote.js
    - These components were not working properly because of the previous update

--- frontend/src/features/notes/EditNote.js
+++ frontend/src/features/notes/EditNote.js
@@ -1,15 +1,23 @@
 import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectNoteById } from './notesApiSlice'
-import { selectAllUsers } from '../users/usersApiSlice'
 import EditNoteForm from './EditNoteForm'
+import { useGetUsersQuery } from '../users/usersApiSlice'
+import { useGetNotesQuery } from './notesApiSlice'
 
 const EditNote = () => {
 
     const { id } = useParams()
 
-    const note = useSelector(state => selectNoteById(state, id))
-    const users = useSelector(selectAllUsers)
+    const { note } = useGetNotesQuery('notesList', {
+        selectFromResult:({ data }) => ({
+            note: data?.entities[id]
+        })
+    })
+
+    const { users } = useGetUsersQuery('usersList', {
+        selectFromResult:({ data }) => ({
+            users: data?.ids.map(id => data?.entities[id])
+        })
+    })
 
     const content = note && users ? <EditNoteForm note={note} users={users} /> : <p>Loading...</p>
 
--- frontend/src/features/users/EditUser.js
+++ frontend/src/features/users/EditUser.js
@@ -1,12 +1,15 @@
 import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectUserById } from './usersApiSlice'
 import EditUserForm from './EditUserForm'
+import { useGetUsersQuery } from './usersApiSlice'
 
 const EditUser = () => {
     const { id } = useParams()
 
-    const user = useSelector(state => selectUserById(state, id))
+    const { user } = useGetUsersQuery('usersList', {
+        selectFromResult: ({ data }) => ({
+            user: data?.entities[id]
+        })
+    })
 
     const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>
 
